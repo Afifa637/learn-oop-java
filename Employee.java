075e3b4182caf9005c96a37d7e89cc452b4363a3
Afifa Sultana <afifasultana637@gmail.com>
2025-08-21 19:59:09 +0600

    Added Employee class with getters, setters and constructor

---
+++ Employee.java
@@ -0,0 +1,32 @@
+public class Employee {
+    private String name;
+    private int age;
+    private int id;
+
+    //constructors
+    public Employee(String name, int age, int id){
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+    //getters
+    public String getName(){ 
+        return name; 
+    }
+    public int getAge(){ 
+        return age; 
+    }
+    public int getIDNo() { 
+        return id; 
+    }
+    //setters
+    public void setName(String name) { 
+        this.name = name; 
+    }
+    public void setAge(int age) { 
+        this.age = age; 
+    }
+    public void setIDNo(int id) { 
+        this.id = id; 
+    }
+}
